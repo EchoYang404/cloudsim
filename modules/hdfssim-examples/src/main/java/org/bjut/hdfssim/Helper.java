690318e1cc18e854458b29f8e08fe085f196d753
EchoYang33 <echo_register@163.com>
2017-01-03 16:02:34 +0800

    add Helper

---
+++ modules/hdfssim-examples/src/main/java/org/bjut/hdfssim/Helper.java
@@ -0,0 +1,22 @@
+package org.bjut.hdfssim;
+
+import org.bjut.hdfssim.models.HDFS.Datanode;
+import org.bjut.hdfssim.models.HDFS.Namenode;
+import java.util.*;
+
+public class Helper {
+    public static void printStorageUsage(Namenode namenode) {
+        Iterator<Map.Entry<Integer, List<Datanode>>> entryIterator = namenode.getDatanodeList().entrySet().iterator();
+        while (entryIterator.hasNext()) {
+            Map.Entry<Integer, List<Datanode>> entry = entryIterator.next();
+            Iterator<Datanode> datanodeIterator = entry.getValue().iterator();
+            while (datanodeIterator.hasNext()) {
+                Datanode datanode = datanodeIterator.next();
+                System.out.println("RId " + entry.getKey() + " DId " + datanode.getId() + " SSD " + datanode.getSSDStorageUsage());
+                System.out.println("RId " + entry.getKey() + " DId " + datanode.getId() + " HDD " + datanode.getHDDStorageUsage());
+            }
+        }
+    }
+
+
+}
