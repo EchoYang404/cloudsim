c21f48570be1e655dfa7df61abdaa20e54639abf
EchoYang33 <echo_register@163.com>
2017-01-03 15:51:05 +0800

    modify DatanodeType

--- modules/hdfssim/src/main/java/org/bjut/hdfssim/models/HDFS/DatanodeType.java
+++ modules/hdfssim/src/main/java/org/bjut/hdfssim/models/HDFS/DatanodeType.java
@@ -2,23 +2,29 @@ package org.bjut.hdfssim.models.HDFS;
 
 public class DatanodeType {
     //public int id;
-    public double mips;
+    public double mips; // MillionInstructions/s
     public int cores;
-    public int ram;
-    public double hddCapacity;
-    public double ssdCapacity;
+    public int ram; //MB
+    public double bw; // MB/s
+    public double hddCapacity; // MB
+    public double hddMaxTransferRate;// MB/s
+    public double ssdCapacity; //MB
+    public double ssdMaxTransferRate;// MB/s
 
     public DatanodeType()
     {
 
     }
 
-    public DatanodeType(double mips, int cores, int ram, double hddCapacity, double ssdCapacity)
+    public DatanodeType(double mips, int cores, int ram, double bw, double hddCapacity, double hddMaxTransferRate, double ssdCapacity, double ssdMaxTransferRate)
     {
         this.mips = mips;
         this.cores = cores;
         this.ram = ram;
+        this.bw = bw;
         this.hddCapacity = hddCapacity;
         this.ssdCapacity = ssdCapacity;
+        this.ssdMaxTransferRate = ssdMaxTransferRate;
+        this.hddMaxTransferRate = hddMaxTransferRate;
     }
 }
