8efebbf9bf5d3db6667ff00f3edd1dc260c75c55
EchoYang33 <echo_register@163.com>
2017-01-03 16:02:01 +0800

    delete useless classes

--- modules/hdfssim/src/main/java/org/bjut/hdfssim/models/Request/Request.java
+++
@@ -1,9 +0,0 @@
-package org.bjut.hdfssim.models.Request;
-
-import org.cloudbus.cloudsim.core.SimEvent;
-
-public class Request extends SimEvent {
-    public int id;
-    public double submissionTime;
-    public Job job;
-}
