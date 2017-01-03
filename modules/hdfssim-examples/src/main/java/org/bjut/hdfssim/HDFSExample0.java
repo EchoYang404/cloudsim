9fa1de7128b46612f5818eeadb6d8334b03a1c54
EchoYang33 <echo_register@163.com>
2017-01-03 16:02:23 +0800

    test HDFS

--- modules/hdfssim-examples/src/main/java/org/bjut/hdfssim/HDFSExample0.java
+++ modules/hdfssim-examples/src/main/java/org/bjut/hdfssim/HDFSExample0.java
@@ -14,58 +14,13 @@ public class HDFSExample0 {
     public static void main(String[] args) {
         // First step : Create namenode
         Namenode namenode = new Namenode();
-        // Second step : Create files
-        List<HFile> fileList = createHFileByRadom(1000,1024,2048,namenode);
-        // Third step : Add datanodes to namenode
-        int rackNum = 10;
-        int datanodeNum = 100;
-        Random random = new Random();
-        for (int i = 0; i < datanodeNum; i++) {
-            int rackId = random.nextInt(10);
-            namenode.addDatanode(rackId,new Datanode(rackId, 1048578,524288));
-        }
-        // Fourth step: Upload files to datanode
-        namenode.setHFileList(fileList);
+        // Second step : Create datanodes from Config Files
+        namenode.createDatanodeFromConfig(Configuration.getBasePath() + "HDFSConfig.json");
+        // Third step : Create files and upload files
+        namenode.createHFileByRadom(1000,1024,2048);
 
-//        String path = Configuration.getBasePath() + "hdfs.txt";
-//        try {
-//            ObjectOutputStream oout = new ObjectOutputStream(new FileOutputStream(path));
-//            oout.writeObject(namenode);
-//            oout.close();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-        printStorageUsage(namenode);
-    }
+        Map<Integer,HDFSHost> hostList = namenode.getHDFSHostList(); // datanodeId, HDFSHost
 
-    private static List<HFile> createHFileByRadom(int fileNum, int minSize, int maxSize, Namenode namenode)
-    {
-        List<HFile> fileList = new ArrayList<>();
-        Random random = new Random();
-
-        for(int i = 0; i < fileNum; i++)
-        {
-            double size = random.nextInt(maxSize - minSize) + minSize;
-            HFile file = new HFile(size,namenode);
-            fileList.add(file);
-        }
-        return fileList;
-    }
-
-    private static void printStorageUsage(Namenode namenode)
-    {
-        Iterator<Map.Entry<Integer, List<Datanode>>> entryIterator = namenode.getDatanodeList().entrySet().iterator();
-        while (entryIterator.hasNext())
-        {
-            Map.Entry<Integer, List<Datanode>> entry = entryIterator.next();
-            Iterator<Datanode> datanodeIterator = entry.getValue().iterator();
-            while(datanodeIterator.hasNext())
-            {
-                Datanode datanode = datanodeIterator.next();
-                System.out.println("RId " + entry.getKey() + " DId " + datanode.getId() + " SSD " +datanode.getSSDStorageUsage());
-                System.out.println("RId " + entry.getKey() + " DId " + datanode.getId() + " HDD " +datanode.getHDDStorageUsage());
-            }
-        }
-
+        Helper.printStorageUsage(namenode);
     }
 }
